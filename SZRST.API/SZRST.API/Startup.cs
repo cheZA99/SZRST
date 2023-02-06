5cdcb71fd95f63d4792eaeb03a5cc2bc9f828c36
zela <amerzelic08@gmail.com>
2023-02-06 23:51:41 +0100

    project startup init, efc

--- SZRST.API/SZRST.API/Startup.cs
+++ SZRST.API/SZRST.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SZRST.API.Models;
 
 namespace SZRST.API
 {
@@ -28,6 +30,7 @@ namespace SZRST.API
         {
 
             services.AddControllers();
+            services.AddDbContext<SZRSTContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SZRSTDb")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SZRST.API", Version = "v1" });
