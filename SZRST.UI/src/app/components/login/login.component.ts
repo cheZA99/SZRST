e9d67a6100b5cdb76824e7d31a31584d3ba4307b
zela <amerzelic08@gmail.com>
2023-02-08 00:23:34 +0100

    prijava i registracija init

---
+++ SZRST.UI/src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
+    this.type = this.isText ? 'text' : 'password';
+  }
+}
---
+++ SZRST.UI/src/app/components/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa-eye-slash';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.eyeIcon = this.isText ? 'fa-eye' : 'fa-eye-slash';
+    this.type = this.isText ? 'text' : 'password';
+  }
+}
