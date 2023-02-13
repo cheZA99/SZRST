ca83cc7ec6c10e9fd9c92e162a1a32e2a8237494
zela <amerzelic08@gmail.com>
2023-02-14 00:22:50 +0100

    password hash, checking existing email and username. regex

--- SZRST.API/SZRST.API/Controllers/KorisnikController.cs
+++ SZRST.API/SZRST.API/Controllers/KorisnikController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using SZRST.API.Context;
+using SZRST.API.Helpers;
 using SZRST.API.Models;
 
 namespace SZRST.API.Controllers
@@ -40,6 +44,20 @@ namespace SZRST.API.Controllers
         {
             if (userObj == null)
                 return BadRequest();
+
+            if (await CheckUserNameExistAsync(userObj.KorisnickoIme))
+                return BadRequest(new { Message = "Korisničko Ime već postoji!" });
+
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email već postoji!" });
+
+            var passwordCheck = CheckPasswordStrength(userObj.Lozinka);
+            if (!string.IsNullOrEmpty(passwordCheck))
+                return BadRequest(new { Message = passwordCheck.ToString() });
+
+            userObj.Lozinka = PasswordHash.HashPassword(userObj.Lozinka);
+            userObj.Rola = "Admin";
+            userObj.Token = "123";
             await dbContext.AddAsync(userObj);
             await dbContext.SaveChangesAsync();
             return Ok( new
@@ -47,6 +65,30 @@ namespace SZRST.API.Controllers
                 Message = "Korisnik uspješno registrovan!"
             });
 
+        }
+
+        private Task<bool> CheckUserNameExistAsync(string korisnickoIme)
+        {
+             return dbContext.Korisnik.AnyAsync(x => x.KorisnickoIme == korisnickoIme);
+        }
+
+        private Task<bool> CheckEmailExistAsync(string email)
+        {
+            return dbContext.Korisnik.AnyAsync(x => x.Email == email);
+        }
+
+        private string CheckPasswordStrength(string password)
+        {
+            string specialCharRegex = "[!,#,$,<,>,%,&,/,(,),=,?,*.+,-,_]";
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8)
+                sb.Append("Lozinka treba da sadrži minimalno 8 znakova!" + Environment.NewLine);
+            if(!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("Lozinka treba da sadrži alfanumeričke znakove!" + Environment.NewLine);
+            if (!Regex.IsMatch(password, specialCharRegex))
+                sb.Append("Lozinka treba da sadrži sepcijalni karakter!" + Environment.NewLine);
+
+            return sb.ToString();
         }
     }
 }
