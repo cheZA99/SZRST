422e606a82d7b2d87e3b99ed40717997a891e1ef
denijal.jamakovic <deni99jam@gmail.com>
2025-12-16 16:11:57 +0100

    added new minor changes

--- SZRST.UI/src/app/app.component.ts
+++ SZRST.UI/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SZRST.UI';
-}
+  
+
+  ngOnInit() {
+  }
+
+  }
--- SZRST.UI/src/app/services/auth.service.ts
+++ SZRST.UI/src/app/services/auth.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../types/user';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private baseUrl: string = 'https://localhost:5001/api/Auth/';
+  currentUser = signal<User | null>(null);
+
   constructor(private http: HttpClient) {}
 
   signUp(userObj: any) {
@@ -13,6 +17,17 @@ export class AuthService {
   }
 
   login(loginObj: any) {
-    return this.http.post<any>(`${this.baseUrl}login`, loginObj);
+    return this.http.post<any>(`${this.baseUrl}login`, loginObj).pipe(
+      tap(user => {
+        if (user)
+          localStorage.setItem('user', JSON.stringify(user))
+          this.currentUser.set(user)
+      })
+    ) ;
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
