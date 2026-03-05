d48ae3c5e73ad60d56558aa842f8cdbc57e65b4c
cheZA99 <amerzelic08@gmail.com>
2026-03-05 20:28:31 +0100

    Revert "fixed connection string in factory"
    This reverts commit f58b6defe82de041373a2a7e56eae369a14d054e.

--- SZRST.API/SZRST.Infrastructure/Persistance/SZRSTContextFactory .cs
+++ SZRST.API/SZRST.Infrastructure/Persistance/SZRSTContextFactory .cs
@@ -2,32 +2,25 @@
 using Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 
-public class SZRSTContextFactory : IDesignTimeDbContextFactory<SZRSTContext>
+public class SZRSTContextFactory :IDesignTimeDbContextFactory<SZRSTContext>
 {
-    public SZRSTContext CreateDbContext(string[] args)
-    {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false)
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("SZRST");
-
-        var optionsBuilder = new DbContextOptionsBuilder<SZRSTContext>();
-        optionsBuilder.UseSqlServer(connectionString);
+	public SZRSTContext CreateDbContext(string[] args)
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<SZRSTContext>();
+        optionsBuilder.UseSqlServer(
+    "Server=.;Database=SZRST-app-v2;Trusted_Connection=True;TrustServerCertificate=True;"
+);
 
         var tenantProvider = new DummyTenantProvider();
 
-        return new SZRSTContext(optionsBuilder.Options, tenantProvider);
-    }
+		return new SZRSTContext(optionsBuilder.Options, tenantProvider);
+	}
 }
 
-public class DummyTenantProvider : ITenantProvider
+public class DummyTenantProvider :ITenantProvider
 {
-    public int TenantId => 1;
+	public int TenantId => 1;
 
-    public bool IsSuperAdminOrUser => false;
+	public bool IsSuperAdminOrUser => false;
 }
