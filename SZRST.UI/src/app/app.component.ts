d5b828949ec5e3359a806d5b3b9e417cdb77ab17
denijal.jamakovic <deni99jam@gmail.com>
2026-03-06 19:14:28 +0100

    Angular Animations for Smooth Transitions for all pages

--- SZRST.UI/src/app/app.component.ts
+++ SZRST.UI/src/app/app.component.ts
@@ -2,11 +2,20 @@ import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { ThisReceiver } from '@angular/compiler';
 import { Router } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  trigger('routeAnimations', [
+    transition('* <=> *', [
+      style({ opacity: 0 }),
+      animate('200ms ease-in', style({ opacity: 1 }))
+    ])
+  ])
+]
 })
 export class AppComponent implements OnInit {
   protected router = inject(Router);
